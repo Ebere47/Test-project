d946e98325745f7896ebb6c2d156221835baf7c0
Zurum <chizurumeberechi1@gmail.com>
2026-09-23 15:41:56 +0100

    change package

--- src/main/java/Student/java/Studentservice.java
+++
@@ -1,6 +0,0 @@
-//package Service;
-//public class studentService {
-//    public void addStudent(Student student){
-//        System.out.println(student.getName());
-//    }
-//}
---
+++ src/main/java/com/example/demo/Studentservice.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import Student.Student;
+
+public class Studentservice {
+    public void addStudent(Student student){
+        System.out.println(student.getName());
+    }
+
+}
+
+class NewStudentService{
+
+}
+
